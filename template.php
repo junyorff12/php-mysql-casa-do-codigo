b9921248923737e4d9531fc80dbf6e4813f5007e
junyorff12 <junyorff12@gmail.com>
2018-05-28 17:44:23 -0300

    arquivo css adicionado

--- template.php
+++ template.php
@@ -13,18 +13,52 @@
                     Tarefa:
                     <input type="text" name="nome" />
                 </label>
+               <label>
+                Descrição (Opcional):
+                    <textarea name="descricao"></textarea>
+                </label>
+                <label>
+                    Prazo (Opcional):
+                    <input type="text" name="prazo" />
+                </label>
+                <fieldset>
+                    <legend>Prioridade:</legend>
+                    <label>
+                        <input type="radio" name="prioridade" value="baixa" checked />
+                        
+                        Baixa
+                        <input type="radio" name="prioridade" value="media" />
+                        Média
+                        <input type="radio" name="prioridade" value="alta" />
+                        Alta
+                    </label>
+                </fieldset>
+                <label>
+                    Tarefa concluída:
+                    <input type="checkbox" name="concluida" value="sim" />
+                </label>
+                
+
                 <input type="submit" value="Cadastrar" />
             </fieldset>
         </form>
         <table>
             <tr>
-                <th>Tarefas</th>
+                <th>Tarefa</th>
+                <th>Descricao</th>
+                <th>Prazo</th>
+                <th>Prioridade</th>
+                <th>Concluída</th>
             </tr>
-        <?php foreach ($lista_tarefas as $tarefa) : ?>
-            <tr>
-                <td><?php echo $tarefa; ?> </td>
-            </tr>
-        <?php endforeach; ?>
-        </table>
+            <?php foreach ($lista_tarefas as $tarefa) : ?>
+                <tr>
+                    <td><?php echo $tarefa['nome']; ?> </td>
+                    <td><?php echo $tarefa['descricao']; ?> </td>
+                    <td><?php echo $tarefa['prazo']; ?> </td>
+                    <td><?php echo $tarefa['prioridade']; ?> </td>
+                    <td><?php echo $tarefa['concluida']; ?> </td>
+                </tr>
+            <?php endforeach; ?>
+    </table>
     </body>
 </html>
